e9133097e1390903431cd7ae3f9e9b96c60571d3
Sergi Moreno <sergimoreno5b@gmail.com>
2021-02-24 18:06:07 +0100

    Delokos

---
+++ bloques.c
@@ -0,0 +1,38 @@
+#include "bloques.h"
+
+int descriptor;
+
+int bmount(const char *camino) {
+    descriptor = open(camino, O_RDWR|O_CREAT, 0666);
+    
+    if (descriptor == -1) {
+        // error
+        perror("Error!");
+        return -1;
+    }
+
+    return descriptor;
+}
+
+int bumount() {
+    if (close(descriptor) == -1) {
+        // error
+        printf("Error!");
+        return -1;
+    }
+    
+    return 0;
+}
+
+int bwrite(unsigned int nbloque, const void *buf) {
+    off_t desplazamiento = nbloque * BLOCKSIZE;
+    if (lseek(descriptor,desplazamiento,SEEK_SET) != -1) {
+        unsigned int nbytes = (unsigned int)sizeof(*buf);
+        if (write(descriptor,buf,nbytes) >= 0) {
+            return BLOCKSIZE;
+        }
+    }
+    
+    return -1;
+}
+
