7c8d700ae76960ec669f4e71ac16dcf436115c79
Roch Dawid <david.roch.2001@gmail.com>
2021-03-03 18:29:09 +0100

    más comentarios

--- Nivel 1/mi_mkfs.c
+++ Nivel 1/mi_mkfs.c
@@ -5,14 +5,13 @@ int main(int argc, char **argv) {
 
     if (bmount(argv[1]) != -1) {
         unsigned char buf[BLOCKSIZE];
-        memset(buf,0,BLOCKSIZE);
+        memset(buf,0,BLOCKSIZE); // inicializar a 0 el buffer
         for (int i = 0;i < nbloques;i++) {
-            if (bwrite(i,buf) == -1) {
-                return -1;
+            if (bwrite(i,buf) == -1) { // si da error escribiendo
+                return -1; // lo devolvemos
             }
         }
 
         return bumount();
     }
-
 }
