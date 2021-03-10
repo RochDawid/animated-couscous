be18b245e82d81a17e302f01a68b09bf2c46d62e
Roch Dawid <david.roch.2001@gmail.com>
2021-03-10 15:20:43 +0100

    Se vienen cositas

--- Nivel 2/leer_sf.c
+++ Nivel 2/leer_sf.c
@@ -4,32 +4,38 @@ int main() {
     int descriptor = bmount("disco");
     struct superbloque sb;
     struct inodo in;
-    void *buf;
     
     if (descriptor != -1) {
         for (int i=0;i<tamSB; i++) {
-            bread(i,buf);
+            bread(i,&sb);
         }
-        // sb = buf;
-        printf("DATOS DEL SUPERBLOQUE");
-        printf("posPrimerBloqueMB = ",sb.posPrimerBloqueMB);
-        printf("posUltimoBloqueMB = ",sb.posUltimoBloqueMB);
-        printf("posPrimerBloqueAI = ",sb.posPrimerBloqueAI);
-        printf("posUltimoBloqueAI = ",sb.posUltimoBloqueAI);
-        printf("posPrimerBloqueDatos = ",sb.posPrimerBloqueDatos);
-        printf("posUltimoBloqueDatos = ",sb.posUltimoBloqueDatos);
-        printf("posInodoRaiz = ",sb.posInodoRaiz);
-        printf("posPrimerInodoLibre = ",sb.posPrimerInodoLibre);
-        printf("cantBloquesLibres = ",sb.cantBloquesLibres);
-        printf("cantInodosLibres = ",sb.cantInodosLibres);
-        printf("totBloques = ",sb.totBloques);
-        printf("totInodos = ",sb.totInodos);
+        for (int i=sb.posPrimerBloqueAI;i<sb.posUltimoBloqueAI; i++) {
+            bread(i,&in);
+        }
+        printf("DATOS DEL SUPERBLOQUE\n");
+        printf("posPrimerBloqueMB = %d\n",sb.posPrimerBloqueMB);
+        printf("posUltimoBloqueMB = %d\n",sb.posUltimoBloqueMB);
+        printf("posPrimerBloqueAI = %d\n",sb.posPrimerBloqueAI);
+        printf("posUltimoBloqueAI = %d\n",sb.posUltimoBloqueAI);
+        printf("posPrimerBloqueDatos = %d\n",sb.posPrimerBloqueDatos);
+        printf("posUltimoBloqueDatos = %d\n",sb.posUltimoBloqueDatos);
+        printf("posInodoRaiz = %d\n",sb.posInodoRaiz);
+        printf("posPrimerInodoLibre = %d\n",sb.posPrimerInodoLibre);
+        printf("cantBloquesLibres = %d\n",sb.cantBloquesLibres);
+        printf("cantInodosLibres = %d\n",sb.cantInodosLibres);
+        printf("totBloques = %d\n",sb.totBloques);
+        printf("totInodos = %d\n",sb.totInodos);
 
-        printf("sizeof struct superbloque: ",sizeof(sb));
-        printf("sizeof struct inodo: ",sizeof(in));
+        printf("sizeof struct superbloque: %d\n",sizeof(sb));
+        printf("sizeof struct inodo: %d\n",sizeof(in));
 
-        printf("RECORRIDO LISTA ENLAZADA DE INODOS LIBRES");
+        printf("\nRECORRIDO LISTA ENLAZADA DE INODOS LIBRES\n");
 
+        struct inodo arInodos[sb.cantInodosLibres];
+        for (int i=sb.posPrimerInodoLibre;i<(sb.posPrimerInodoLibre+sb.cantInodosLibres);i++) {
+            bread(i,arInodos);
+            printf(" %d ",arInodos[i-sb.posPrimerInodoLibre].punterosDirectos[0]);
+        }
     } else {
         perror("Error montando dispositivo virtual");
     }
