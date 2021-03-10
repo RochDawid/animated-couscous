13d21785caa586587f6df8bc9b370f3a2783c4b6
Roch Dawid <david.roch.2001@gmail.com>
2021-03-10 09:01:30 +0100

    Línies 9-12 mi_mkfs comentades per evitar warnings

--- Nivel 2/mi_mkfs.c
+++ Nivel 2/mi_mkfs.c
@@ -6,12 +6,11 @@ int main(int argc, char **argv) {
     if (bmount(argv[1]) != -1) {
         unsigned char buf[BLOCKSIZE];
         memset(buf,0,BLOCKSIZE); // inicializar a 0 el buffer
-        int ninodos = nbloques/4;
-        initSB(nbloques,ninodos);
-        initMB();
-        initAI();
+        //int ninodos = nbloques/4;
+        //initSB(nbloques,ninodos);
+        //initMB();
+        //initAI();
         for (int i = 0;i < nbloques;i++) {
-
             if (bwrite(i,buf) == -1) { // si da error escribiendo
                 return -1; // lo devolvemos
             }
