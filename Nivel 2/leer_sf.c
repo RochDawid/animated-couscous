6b63c80fe2f377db9b53de40eeb8a797715aa460
Roch Dawid <david.roch.2001@gmail.com>
2021-03-10 19:06:37 +0100

    leer_sf corregit

--- Nivel 2/leer_sf.c
+++ Nivel 2/leer_sf.c
@@ -3,13 +3,9 @@
 int main() {
     int descriptor = bmount("disco");
     struct superbloque sb;
-    struct inodo in;
     
     if (descriptor != -1) {
         bread(posSB,&sb);
-        for (int i=sb.posPrimerBloqueAI;i<sb.posUltimoBloqueAI; i++) {
-            bread(i,&in);
-        }
         printf("DATOS DEL SUPERBLOQUE\n");
         printf("posPrimerBloqueMB = %d\n",sb.posPrimerBloqueMB);
         printf("posUltimoBloqueMB = %d\n",sb.posUltimoBloqueMB);
@@ -25,14 +21,19 @@ int main() {
         printf("totInodos = %d\n",sb.totInodos);
 
         printf("sizeof struct superbloque: %d\n",(int) sizeof(sb));
-        printf("sizeof struct inodo: %d\n",(int) sizeof(in));
+        printf("sizeof struct inodo: %d\n",(int) sizeof(struct inodo));
 
         printf("\nRECORRIDO LISTA ENLAZADA DE INODOS LIBRES\n");
 
-        struct inodo arInodos[sb.cantInodosLibres];
-        for (int i=sb.posPrimerInodoLibre;i<(sb.posPrimerInodoLibre+sb.cantInodosLibres);i++) {
-            bread(i,arInodos);
-            printf(" %d ",arInodos[i-sb.posPrimerInodoLibre].punterosDirectos[0]);
+        struct inodo arInodos[BLOCKSIZE/INODOSIZE];
+        for (int i = sb.posPrimerBloqueAI; i <= sb.posUltimoBloqueAI; i++) {
+            if (bread(i,arInodos) == -1) {
+                perror("Error leyendo");
+                return -1;
+            }
+            for (int j = 0; j <= (BLOCKSIZE/INODOSIZE); j++) {
+                printf("%d\n",arInodos[j].punterosDirectos[0]);
+            }
         }
         bumount();
     } else {
