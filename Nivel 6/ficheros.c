8231d800b7f09bbf52767e625cae69b7b90d20e8
Sergi Moreno <sergimoreno5b@gmail.com>
2021-04-04 17:24:36 +0200

    Nivell 6, demanar lo de leer i escribir

---
+++ Nivel 6/ficheros.c
@@ -0,0 +1,210 @@
+#include "ficheros.h"
+
+/*
+    mi_write_f: Escribe el contenido procedente de un buffer de memoria, buf_original, de tamaño nbytes, en un fichero/directorio
+    input: unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes
+    output: 0
+    uses: bread(),bwrite()
+    used by: mi_mkfs(), leer_sf()
+*/
+int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes) {
+    struct inodo inodo;
+    leer_inodo(ninodo, &inodo);
+    if ((inodo.permisos & 2) == 2) {
+        unsigned int primerBL = offset/BLOCKSIZE;
+        unsigned int ultimoBL = (offset + nbytes - 1)/BLOCKSIZE;
+        unsigned int desp1 = offset%BLOCKSIZE;
+        unsigned int desp2 = (offset + nbytes - 1)%BLOCKSIZE;
+        unsigned int nbfisico = traducir_bloque_inodo(ninodo,primerBL,1);
+        printf("1r BL : %d, ultBL : %d, desp1 : %d, desp2 : %d, nbfisico : %d, nbytes : %d\n",primerBL,ultimoBL,desp1,desp2,nbfisico,nbytes);
+        unsigned char buf_bloque[BLOCKSIZE];
+        unsigned int bytesEscritos;
+        bread(nbfisico,buf_bloque);
+        if (primerBL == ultimoBL) { // escribimos en un único bloque
+            memcpy(buf_bloque + desp1, buf_original,nbytes);
+            bwrite(nbfisico,buf_bloque);
+            bytesEscritos = desp2 - desp1;
+            printf("mi_write -> bytesEscritos : %d\n",bytesEscritos);
+        } else { // tenemos que escribir en más de un bloque
+            memcpy(buf_bloque + desp1, buf_original,BLOCKSIZE - desp1);
+            bytesEscritos = bwrite(nbfisico,buf_bloque) - desp1;
+            for (int i = primerBL + 1;i < ultimoBL;i++) {
+                nbfisico = traducir_bloque_inodo(ninodo,i,1);
+                bytesEscritos += bwrite(nbfisico,buf_original + (BLOCKSIZE - desp1) + (i-primerBL-1)*BLOCKSIZE);
+            }
+            nbfisico = traducir_bloque_inodo(ninodo,ultimoBL,1);
+            bread(nbfisico,buf_bloque);
+            memcpy(buf_bloque, buf_original + (nbytes - desp2 - 1),desp2 + 1);
+            bytesEscritos += bwrite(nbfisico,buf_bloque) - (BLOCKSIZE - (desp2 + 1));
+        }
+
+        leer_inodo(ninodo,&inodo);
+        time_t timer;
+        if (bytesEscritos > inodo.tamEnBytesLog) {
+            inodo.tamEnBytesLog = bytesEscritos;
+            inodo.ctime = time(&timer);
+        }
+        
+        inodo.mtime = time(&timer);
+        escribir_inodo(ninodo,inodo);
+            
+        return bytesEscritos;
+    } else {
+        perror("Error: no dispone de permisos para leer el fichero/directorio.");
+        return -1;
+    }
+}
+
+/*
+    mi_read_f: Lee información de un fichero/directorio y la almacena en un buffer de memoria, buf_original
+    input: unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes
+    output: 0
+    uses: bread(),bwrite()
+    used by: mi_mkfs(), leer_sf()
+*/
+int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes) {
+    struct inodo inodo;
+    leer_inodo(ninodo, &inodo);
+    if ((inodo.permisos & 4) == 4) {
+        unsigned int leidos = 0;
+        printf("mi_Read -> offset %d \t inodo.tamBytes : %d\n",offset,inodo.tamEnBytesLog);
+        if (offset >= inodo.tamEnBytesLog) { // no podemos leer nada
+            leidos = 0;
+            return leidos;
+        }
+        if ((offset + nbytes) >= inodo.tamEnBytesLog) { // pretende leer más allá de EOF
+            nbytes = inodo.tamEnBytesLog-offset;
+            // leemos sólo los bytes que podemos desde el offset hasta EOF
+        }
+
+        unsigned int primerBL = offset/BLOCKSIZE;
+        unsigned int ultimoBL = (offset + nbytes - 1)/BLOCKSIZE;
+        unsigned int desp1 = offset%BLOCKSIZE;
+        unsigned int desp2 = (offset + nbytes - 1)%BLOCKSIZE;
+        unsigned int nbfisico = traducir_bloque_inodo(ninodo,primerBL,0);
+        unsigned char buf_bloque[BLOCKSIZE];
+        unsigned int bytesLeidos;
+
+        if (primerBL == ultimoBL) { // escribimos en un único bloque
+            if (nbfisico != -1) {
+                bread(nbfisico,buf_bloque);
+                memcpy(buf_original, buf_bloque + desp1,nbytes);
+            }
+            bytesLeidos = BLOCKSIZE - desp1;
+        } else { // tenemos que escribir en más de un bloque
+            if (nbfisico != -1) {
+                bread(nbfisico,buf_bloque);
+                memcpy(buf_original, buf_bloque + desp1,BLOCKSIZE - desp1);
+            }
+            bytesLeidos = BLOCKSIZE - desp1;
+            for (int i = primerBL + 1;i < ultimoBL;i++) {
+                nbfisico = traducir_bloque_inodo(ninodo,i,0);
+                if (nbfisico != -1) {
+                    bread(nbfisico,buf_bloque);
+                    memcpy(buf_original, buf_bloque,BLOCKSIZE);
+                }
+                bytesLeidos += BLOCKSIZE;
+            }
+            nbfisico = traducir_bloque_inodo(ninodo,ultimoBL,1);
+            if (nbfisico != -1) {
+                bread(nbfisico,buf_bloque);
+                memcpy(buf_original, buf_bloque + (nbytes - desp2 - 1),desp2 + 1);
+            }
+            bytesLeidos += desp2 + 1;
+        }
+
+        leer_inodo(ninodo,&inodo);
+        time_t timer;
+        inodo.atime = time(&timer);
+        escribir_inodo(ninodo,inodo);
+            
+        return bytesLeidos;
+
+    } else {
+        perror("Error: no dispone de permisos para leer el fichero/directorio.");
+        return -1;
+    }
+}
+
+/*
+    mi_stat_f: Devuelve la metainformación de un fichero/directorio
+    input: unsigned int ninodo, struct STAT *p_stat
+    output: 0
+    uses: bread(),bwrite()
+    used by: mi_mkfs(), leer_sf()
+*/
+int mi_stat_f(unsigned int ninodo, struct STAT *p_stat) {
+    struct inodo inodo;
+    leer_inodo(ninodo, &inodo);
+    p_stat->tipo = inodo.tipo;
+    p_stat->atime = inodo.atime;
+    p_stat->ctime = inodo.ctime;
+    p_stat->mtime = inodo.mtime;
+    p_stat->nlinks = inodo.nlinks;
+    p_stat->numBloquesOcupados = inodo.numBloquesOcupados;
+    p_stat->permisos = inodo.permisos;
+    p_stat->tamEnBytesLog = inodo.tamEnBytesLog;
+
+    struct tm *ts;
+    char atime[80];
+    char mtime[80];
+    char ctime[80];
+
+    ts = localtime(&p_stat->atime);
+    strftime(atime, sizeof(atime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&p_stat->mtime);
+    strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&p_stat->ctime);
+    strftime(ctime, sizeof(ctime), "%a %Y-%m-%d %H:%M:%S", ts);
+
+/*     printf("METAINFORMACIÓN DEL FICHERO/DIRECTORIO DEL INODO %d\n", ninodo);
+    printf("tipo: %c\n",p_stat->tipo);
+    printf("permisos: %d\n",p_stat->permisos);
+    printf("atime: %ld\n",p_stat->atime);
+    printf("ctime: %ld\n",p_stat->ctime);
+    printf("mtime: %ld\n",p_stat->mtime);
+    printf("nlinks: %d\n",p_stat->nlinks);
+    printf("tamEnBytesLog: %d\n",p_stat->tamEnBytesLog);
+    printf("numBloquesOcupados: %d\n",p_stat->numBloquesOcupados); */
+    fprintf(stderr,"\nMETAINFORMACIÓN DEL FICHERO/DIRECTORIO DEL INODO %d\n", ninodo);
+    fprintf(stderr,"tipo: %c\n",p_stat->tipo);
+    fprintf(stderr,"permisos: %d\n",p_stat->permisos);
+    fprintf(stderr,"atime: %s\n",atime);
+    fprintf(stderr,"ctime: %s\n",ctime);
+    fprintf(stderr,"mtime: %s\n",mtime);
+    fprintf(stderr,"nlinks: %d\n",p_stat->nlinks);
+    fprintf(stderr,"tamEnBytesLog: %d\n",p_stat->tamEnBytesLog);
+    fprintf(stderr,"numBloquesOcupados: %d\n",p_stat->numBloquesOcupados);
+
+    return 0;
+}
+
+/*
+    mi_chmod_f: Cambia los permisos de un fichero/directorio con el valor que indique el argumento permisos
+    input: unsigned int ninodo, unsigned char permisos
+    output: 0
+    uses: bread(),bwrite()
+    used by: mi_mkfs(), leer_sf()
+*/
+int mi_chmod_f(unsigned int ninodo, unsigned char permisos) {
+    struct inodo inodo;
+    time_t timer;
+    leer_inodo(ninodo, &inodo);
+    inodo.permisos = permisos;
+    inodo.ctime = time(&timer);
+    escribir_inodo(ninodo, inodo);
+
+    return 0;
+}
+
+/*
+    mi_truncar_f: Trunca un fichero/directorio (correspondiente al nº de inodo, ninodo, pasado como argumento) 
+                    a los bytes indicados como nbytes, liberando los bloques necesarios.
+    input: unsigned int ninodo, unsigned int nbytes
+    output: 
+    uses: 
+    used by: 
+*/
+int mi_truncar_f(unsigned int ninodo, unsigned int nbytes) {
+
+}
---
+++ Nivel 6/leer_sf.c
@@ -0,0 +1,109 @@
+#include "ficheros_basico.h"
+
+int main() {
+    bmount("disco");
+    struct superbloque sb;
+
+    
+    bread(posSB,&sb);
+    printf("DATOS DEL SUPERBLOQUE\n");
+    printf("posPrimerBloqueMB = %d\n",sb.posPrimerBloqueMB);
+    printf("posUltimoBloqueMB = %d\n",sb.posUltimoBloqueMB);
+    printf("posPrimerBloqueAI = %d\n",sb.posPrimerBloqueAI);
+    printf("posUltimoBloqueAI = %d\n",sb.posUltimoBloqueAI);
+    printf("posPrimerBloqueDatos = %d\n",sb.posPrimerBloqueDatos);
+    printf("posUltimoBloqueDatos = %d\n",sb.posUltimoBloqueDatos);
+    printf("posInodoRaiz = %d\n",sb.posInodoRaiz);
+    printf("posPrimerInodoLibre = %d\n",sb.posPrimerInodoLibre);
+    printf("cantBloquesLibres = %d\n",sb.cantBloquesLibres);
+    printf("cantInodosLibres = %d\n",sb.cantInodosLibres);
+    printf("totBloques = %d\n",sb.totBloques);
+    printf("totInodos = %d\n",sb.totInodos);
+
+    reservar_inodo('f',6);
+
+    printf("\nINODO 1. TRADUCCION DE LOS BLOQUES LOGICOS 8, 204, 30.004, 400.004 y 468.750\n");
+    int bloquesLog[] = {8, 204, 30004, 400004, 468750};
+    for (int i = 0;i < 5;i++) {
+        traducir_bloque_inodo(1,bloquesLog[i],1);
+    }
+    
+    struct inodo inodo;
+    leer_inodo(1,&inodo);
+    struct tm *ts;
+    char atime[80];
+    char mtime[80];
+    char ctime[80];
+
+    ts = localtime(&inodo.atime);
+    strftime(atime, sizeof(atime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&inodo.mtime);
+    strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&inodo.ctime);
+    strftime(ctime, sizeof(ctime), "%a %Y-%m-%d %H:%M:%S", ts);
+
+    printf("\nDATOS DEL DIRECTORIO RAIZ\n");
+    printf("tipo: %c\n",inodo.tipo);
+    printf("permisos: %d\n",inodo.permisos);
+    printf("atime: %s\n",atime);
+    printf("ctime: %s\n",ctime);
+    printf("mtime: %s\n",mtime);
+    printf("nlinks: %d\n",inodo.nlinks);
+    printf("tamEnBytesLog: %d\n",inodo.tamEnBytesLog);
+    printf("numBloquesOcupados: %d\n",inodo.numBloquesOcupados);
+
+    bread(posSB,&sb);
+    printf("\nSB.posPrimerInodoLibre : %d\n",sb.posPrimerInodoLibre);
+
+/*    printf("\nsizeof struct superbloque: %d\n",(int) sizeof(sb));
+    printf("sizeof struct inodo: %d\n",(int) sizeof(struct inodo));
+
+    printf("\nRECORRIDO LISTA ENLAZADA DE INODOS LIBRES\n");
+
+    struct inodo arInodos[BLOCKSIZE/INODOSIZE];
+    for (int i = sb.posPrimerBloqueAI; i <= sb.posUltimoBloqueAI; i++) {
+        bread(i,arInodos);
+        for (int j = 0; j < (BLOCKSIZE/INODOSIZE); j++) {
+            if (arInodos[j].tipo == 'l') {
+                printf("%d ",arInodos[j].punterosDirectos[0]);
+            }
+        }
+    }
+    printf("\n");
+
+    unsigned int nbloque = reservar_bloque();
+
+    bread(posSB,&sb);
+    printf("DATOS DEL SUPERBLOQUE\n");
+    printf("posPrimerBloqueMB = %d\n",sb.posPrimerBloqueMB);
+    printf("posUltimoBloqueMB = %d\n",sb.posUltimoBloqueMB);
+    printf("posPrimerBloqueAI = %d\n",sb.posPrimerBloqueAI);
+    printf("posUltimoBloqueAI = %d\n",sb.posUltimoBloqueAI);
+    printf("posPrimerBloqueDatos = %d\n",sb.posPrimerBloqueDatos);
+    printf("posUltimoBloqueDatos = %d\n",sb.posUltimoBloqueDatos);
+    printf("posInodoRaiz = %d\n",sb.posInodoRaiz);
+    printf("posPrimerInodoLibre = %d\n",sb.posPrimerInodoLibre);
+    printf("cantBloquesLibres = %d\n",sb.cantBloquesLibres);
+    printf("cantInodosLibres = %d\n",sb.cantInodosLibres);
+    printf("totBloques = %d\n",sb.totBloques);
+    printf("totInodos = %d\n",sb.totInodos);
+
+    liberar_bloque(nbloque);
+
+    bread(posSB,&sb);
+    printf("DATOS DEL SUPERBLOQUE\n");
+    printf("posPrimerBloqueMB = %d\n",sb.posPrimerBloqueMB);
+    printf("posUltimoBloqueMB = %d\n",sb.posUltimoBloqueMB);
+    printf("posPrimerBloqueAI = %d\n",sb.posPrimerBloqueAI);
+    printf("posUltimoBloqueAI = %d\n",sb.posUltimoBloqueAI);
+    printf("posPrimerBloqueDatos = %d\n",sb.posPrimerBloqueDatos);
+    printf("posUltimoBloqueDatos = %d\n",sb.posUltimoBloqueDatos);
+    printf("posInodoRaiz = %d\n",sb.posInodoRaiz);
+    printf("posPrimerInodoLibre = %d\n",sb.posPrimerInodoLibre);
+    printf("cantBloquesLibres = %d\n",sb.cantBloquesLibres);
+    printf("cantInodosLibres = %d\n",sb.cantInodosLibres);
+    printf("totBloques = %d\n",sb.totBloques);
+    printf("totInodos = %d\n",sb.totInodos); */
+    
+    return bumount();
+}
