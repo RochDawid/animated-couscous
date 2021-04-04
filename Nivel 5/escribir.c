7931d1b2b961b98812d409fde6c5766978ff03f3
Sergi Moreno <sergimoreno5b@gmail.com>
2021-04-04 12:20:35 +0200

    Makefile i demás

--- Nivel 5/escribir.c
+++ Nivel 5/escribir.c
@@ -1,17 +1,17 @@
 #include "ficheros.h"
 
-int main(int argc, char **argv) {
+int main() {
     bmount("disco");
     unsigned int ninodo = reservar_inodo('f',6);
-    unsigned int arrayOffset = {9000,209000,30725000,409605000,480000000};
-    const char buffer[BLOCKSIZE];
+    unsigned int arrayOffset [] = {9000,209000,30725000,409605000,480000000};
+    char buffer[BLOCKSIZE];
     strcpy(buffer, "lo que siga");
     int length = strlen("lo que siga");
-    mi_write_f(ninodo,buffer,arrayOffset,length);
-    memset(buffer,0,BLOCKSIZE);
-    mi_read_f(ninodo,buffer,arrayOffset,length);
-    printf("Buffer : %s\n",buffer);
-    struct STAT *state;
-    mi_stat_f(ninodo,state);
+    mi_write_f(ninodo,buffer,arrayOffset[0],length);
+    char buffer_lec[BLOCKSIZE];
+    mi_read_f(ninodo,buffer_lec,arrayOffset[0],length);
+    printf("Buffer : %s\n",buffer_lec);
+    struct STAT state;
+    mi_stat_f(ninodo,&state);
     return bumount();
 }
--- Nivel 5/ficheros.c
+++ Nivel 5/ficheros.c
@@ -109,23 +109,23 @@ int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsi
 }
 
 int mi_stat_f(unsigned int ninodo, struct STAT *p_stat) {
-    struct inodo *inodo;
-    leer_inodo(ninodo, inodo);
-    p_stat->tipo = inodo->tipo;
-    p_stat->atime = inodo->atime;
-    p_stat->ctime = inodo->ctime;
-    p_stat->mtime = inodo->mtime;
-    p_stat->nlinks = inodo->nlinks;
-    p_stat->numBloquesOcupados = inodo->numBloquesOcupados;
-    p_stat->permisos = inodo->permisos;
-    p_stat->tamEnBytesLog = inodo->tamEnBytesLog;
+    struct inodo inodo;
+    leer_inodo(ninodo, &inodo);
+    p_stat->tipo = inodo.tipo;
+    p_stat->atime = inodo.atime;
+    p_stat->ctime = inodo.ctime;
+    p_stat->mtime = inodo.mtime;
+    p_stat->nlinks = inodo.nlinks;
+    p_stat->numBloquesOcupados = inodo.numBloquesOcupados;
+    p_stat->permisos = inodo.permisos;
+    p_stat->tamEnBytesLog = inodo.tamEnBytesLog;
 
 /*     printf("METAINFORMACIÓN DEL FICHERO/DIRECTORIO DEL INODO %d\n", ninodo);
     printf("tipo: %c\n",p_stat->tipo);
     printf("permisos: %d\n",p_stat->permisos);
-    printf("atime: %s\n",p_stat->atime);
-    printf("ctime: %s\n",p_stat->ctime);
-    printf("mtime: %s\n",p_stat->mtime);
+    printf("atime: %ld\n",p_stat->atime);
+    printf("ctime: %ld\n",p_stat->ctime);
+    printf("mtime: %ld\n",p_stat->mtime);
     printf("nlinks: %d\n",p_stat->nlinks);
     printf("tamEnBytesLog: %d\n",p_stat->tamEnBytesLog);
     printf("numBloquesOcupados: %d\n",p_stat->numBloquesOcupados); */
