ca5ece4a02e04c568416332110a20748d13f69a7
Sergi Moreno <sergimoreno5b@gmail.com>
2021-04-17 15:09:05 +0200

    Contador de bytes leidos

--- Nivel 5/leer.c
+++ Nivel 5/leer.c
@@ -3,37 +3,33 @@
 int main(int argc,char **argv) { //VIGILAR SA SINTAXIS
     if (argv[1]) {
         bmount(argv[1]);
-        //unsigned int ninodo = reservar_inodo('f',6);
         unsigned int ninodo = atoi(argv[2]);
         fprintf(stderr,"ninodo : %d\n",ninodo);
-        int offset = 0, tambuffer = 1500;
+        int offset = 0,contadorLeidos = 0,leidos = 0, tambuffer = 1500;
         char buffer[tambuffer];
         FILE *fichero;
         char string[128];
 
-        fprintf(stderr, "Abans fopen");
         fichero = fopen(argv[4],"w");
-        fprintf(stderr, "Després fopen");
         memset(buffer,0,tambuffer);
-        fprintf(stderr, "Abans mi_read_f");
-        int leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
-        fprintf(stderr, "Després mi_read_f");
+        leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
+        contadorLeidos = leidos;
         while (leidos > 0) {
-            //fputs(buffer,fichero);
             fwrite(buffer,sizeof(char),sizeof(buffer),fichero);
             write(1,buffer,leidos);
             offset += tambuffer;
             memset(buffer,0,tambuffer);
             leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
-            //leidos == tamEnByteLog
+            contadorLeidos += leidos;
         }
         fclose(fichero);
-        /* struct inodo inodo;
+        struct inodo inodo;
         leer_inodo(ninodo,&inodo);
-        printf("Número bytes leídos : %d\n",numBytesLeidos);
-        printf("tamEnBytesLog : %d\n",inodo.tamEnBytesLog); */
-        sprintf(string,"bytes leidos : %d\n",leidos);
+         
+        sprintf(string,"bytes leidos : %d\n",contadorLeidos);
         write(2,string,strlen(string));
+
+        fprintf(stderr,"tamEnBytesLog : %d\n",inodo.tamEnBytesLog);
         bumount();
 
         return ninodo;
