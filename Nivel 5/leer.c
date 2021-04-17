fa26784620381e587e79b701f4804ca40cd94eb6
Sergi Moreno Pérez <sergimoreno5b@gmail.com>
2021-04-17 17:14:58 +0200

    truncar.c

--- Nivel 5/leer.c
+++ Nivel 5/leer.c
@@ -5,35 +5,38 @@ int main(int argc,char **argv) { //VIGILAR SA SINTAXIS
         bmount(argv[1]);
         //unsigned int ninodo = reservar_inodo('f',6);
         unsigned int ninodo = atoi(argv[2]);
-        fprintf(stderr,"ninodo : %d\n",ninodo);
-        int offset = 0, tambuffer = 1500;
+        //fprintf(stderr,"ninodo : %d\n",ninodo);
+        int offset = 0, leidos = 0, contadorLeidos = 0, tambuffer = 1500;
         char buffer[tambuffer];
         FILE *fichero;
         char string[128];
 
-        fprintf(stderr, "Abans fopen");
-        fichero = fopen(argv[4],"w");
-        fprintf(stderr, "Després fopen");
+        if (argv[3] && argv[4]) {
+            fichero = fopen(argv[4],"w");
+        }
+        //fprintf(stderr, "Després fopen");
         memset(buffer,0,tambuffer);
-        fprintf(stderr, "Abans mi_read_f");
-        int leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
-        fprintf(stderr, "Després mi_read_f");
+        leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
+        contadorLeidos = leidos;
         while (leidos > 0) {
             //fputs(buffer,fichero);
-            fwrite(buffer,sizeof(char),sizeof(buffer),fichero);
-            write(1,buffer,leidos);
+            //fwrite(buffer,sizeof(char),sizeof(buffer),fichero);
             offset += tambuffer;
+            write(1,buffer,leidos);
+            
             memset(buffer,0,tambuffer);
             leidos = mi_read_f(ninodo,buffer,offset,tambuffer);
-            //leidos == tamEnByteLog
+            contadorLeidos += leidos;
         }
+        if (argv[3] && argv[4]) {
         fclose(fichero);
-        /* struct inodo inodo;
+        }
+        struct inodo inodo;
         leer_inodo(ninodo,&inodo);
-        printf("Número bytes leídos : %d\n",numBytesLeidos);
-        printf("tamEnBytesLog : %d\n",inodo.tamEnBytesLog); */
-        sprintf(string,"bytes leidos : %d\n",leidos);
+        
+        sprintf(string,"bytes leidos : %d\n",contadorLeidos);
         write(2,string,strlen(string));
+        fprintf(stderr,"tamEnBytesLog : %d\n",inodo.tamEnBytesLog); 
         bumount();
 
         return ninodo;
