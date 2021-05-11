c48acfadb451f017064ecf1ba3bafaf84de5c4a1
Sergi Moreno Pérez <sergimoreno5b@gmail.com>
2021-05-11 23:03:32 +0200

    Makefile i mi_cat

--- Nivel 9/leer.c
+++ Nivel 9/leer.c
@@ -7,7 +7,7 @@
 #include "directorios.h"
 
 int main(int argc,char **argv) {
-    /* if (argv[1]) {
+    if (argv[1]) {
         if (bmount(argv[1]) < 0) return -1;
         unsigned int ninodo = atoi(argv[2]);
         int offset = 0, leidos = 0, contadorLeidos = 0, tambuffer = 1500;
@@ -49,44 +49,5 @@ int main(int argc,char **argv) {
     }
 
     perror("Sintaxis: leer <nombre_dispositivo>\n");
-    return -1; */
-    //Comprobamos sintaxis
-    if (argc != 5){
-        fprintf(stderr, "Sintaxis: ./leer <disco> </ruta_fichero> \n");
-        exit(-1);
-    }
-    int offset = 0, leidos = 0, contadorLeidos = 0, tambuffer = 1500;
-    char buffer[tambuffer];
-
-    //struct STAT stat;
-    //montamos el dispositivo
-    if (bmount(argv[1]) < 0) return -1;
-
-
-    //obtenemos la ruta y comprobamos que no se refiera a un directorio
-    if (argv[2][strlen(argv[2]) - 1] == '/'){
-        fprintf(stderr, "Error: la ruta se corresponde a un directorio");
-        exit(-1);
-    }
-    char *camino = argv[2];
-    // escribimos varias veces el texto desplazado 1 bloque
-    memset(buffer,0,tambuffer);
-        leidos = mi_read(camino,buffer,offset,tambuffer);
-        if (leidos == -1) return -1;
-        contadorLeidos = leidos;
-
-        while (leidos > 0) {
-            offset += tambuffer;
-            leidos += mi_read(camino, buffer, offset, tambuffer);
-            memset(buffer,0,tambuffer);
-            
-        }
-    fprintf(stderr, "Bytes leidos: %d\n", leidos);
-    /* Visualización del stat
-    mi_stat_f(ninodo, &stat);
-    printf("stat.tamEnBytesLog=%d\n",stat.tamEnBytesLog);
-    printf("stat.numBloquesOcupados=%d\n",stat.numBloquesOcupados);
-    */
-
-    return bumount();
+    return -1;
 }
