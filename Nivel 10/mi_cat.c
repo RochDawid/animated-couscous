b19853335d5f9e001bb3ef7d7d5830e4d9ad298b
Roch Dawid <david.roch.2001@gmail.com>
2021-05-13 18:11:05 +0200

    cosetes

--- Nivel 10/mi_cat.c
+++ Nivel 10/mi_cat.c
@@ -11,10 +11,11 @@ int main(int argc,char **argv) {
         fprintf(stderr, "Sintaxis: ./mi_cat <disco> </ruta_fichero> \n");
         exit(-1);
     }
-    int offset = 0, leidos = 0, contadorLeidos = 0, tambuffer = BLOCKSIZE*4;
+    int offset = 0, leidos = 0, contadorLeidos = 0, tambuffer = BLOCKSIZE*4; //PROBAR tambuffer=1500
     char buffer[tambuffer];
     char string[128];
 
+    //struct STAT stat;
     //montamos el dispositivo
     if (bmount(argv[1]) < 0) return -1;
 
@@ -24,7 +25,6 @@ int main(int argc,char **argv) {
         exit(-1);
     }
     char *camino = argv[2];
-
     // escribimos varias veces el texto desplazado 1 bloque
     memset(buffer,0,tambuffer);
     leidos = mi_read(camino,buffer,offset,tambuffer);
@@ -39,6 +39,10 @@ int main(int argc,char **argv) {
     }
     sprintf(string,"\nbytes leidos : %d\n",contadorLeidos);
     write(2,string,strlen(string));
-
+    /* Visualización del stat
+    mi_stat_f(ninodo, &stat);
+    printf("stat.tamEnBytesLog=%d\n",stat.tamEnBytesLog);
+    printf("stat.numBloquesOcupados=%d\n",stat.numBloquesOcupados);
+    */
     return bumount();
 }
