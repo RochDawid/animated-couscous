5d62eb2b0e6bf4b13c5c1fd47c10cfcc30b087ff
Roch Dawid <david.roch.2001@gmail.com>
2021-05-15 14:14:59 +0200

    mi_ls millorat però no funciona del tot :(

--- Entrega 2/mi_ls.c
+++ Entrega 2/mi_ls.c
@@ -16,11 +16,54 @@ int main(int argc,char **argv) {
         fprintf(stderr, "Error: la ruta se corresponde a un fichero\n");
         return -1;
     }
+
     char buffer[TAMBUFFER];
+    unsigned int p_inodo_dir = 0;
+    unsigned int p_inodo = 0;
+    unsigned int p_entrada = 0;
+    char permisos[3]; 
+    char reservar = 0;
+    struct inodo inodo;
+    int error;
+    char mtime[80];
+    struct tm *ts;
+
+    if ((error = buscar_entrada(argv[2],&p_inodo_dir,&p_inodo,&p_entrada,reservar,6)) < 0) {
+        mostrar_error_buscar_entrada(error);
+        return -1;
+    }
+
+    leer_inodo(p_inodo, &inodo); // leemos el inodo de la entrada
+    
+    if ((inodo.permisos & 6) == 6){
+        strcpy(permisos,"rw-");
+    } else if ((inodo.permisos & 4) == 4) {
+        strcpy(permisos,"r--");
+    } else if ((inodo.permisos & 2) == 2) {
+        strcpy(permisos,"w--");
+    }
+
+
     memset(buffer,0,TAMBUFFER);
     int numEntradas = mi_dir(argv[2],buffer);
     if (numEntradas == -1) return -1;
+
+    ts = localtime(&inodo.mtime);
+    strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
+
     fprintf(stderr,"Total: %d\n",numEntradas);
-    fprintf(stderr,"Contenido buffer: \n %s\n",buffer);
+    fprintf(stderr,"Tipo    Permisos    mTime                       Tamaño  Nombre\n");
+    fprintf(stderr,"--------------------------------------------------------------\n");
+
+    int j = 0;
+    for (int i=0;i<numEntradas;i++) {
+        fprintf(stderr,"%c       %s         %s     %d      ", inodo.tipo,permisos, mtime, inodo.tamEnBytesLog);
+        if (buffer[j] == '\t') j++;
+        for (;j<strlen(buffer) && buffer[j] != '\t';j++) {
+            fprintf(stderr,"%c",buffer[j]);
+        }
+        fprintf(stderr,"\n");
+    }
+
     return bumount();
 }
