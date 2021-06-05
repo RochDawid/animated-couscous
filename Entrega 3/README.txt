cc2c07c9a0199f3ef264c6944325469df203ab32
sermorper13 <sergimoreno5b@gmail.com>
2021-06-05 11:38:39 +0200

    Entrega 3 creada

---
+++ Entrega 3/README.txt
@@ -0,0 +1,26 @@
+* * * * * * * * * * * * * * *
+* Integrantes del grupo DST *
+*			                *
+*   Sergi Moreno Pérez      *
+*   Antoni Payeras Munar    *
+*   Dawid Michal Roch Móll  *
+* * * * * * * * * * * * * * *
+
+Esta práctica consiste en implementar un sistema de ficheros (basado en el ext2 de Unix) en un disco virtual (fichero), el módulo de gestión de 
+ficheros del sistema operativo y la interacción de procesos de usuarios.
+Para ello se ha dividido en 3 entregas. Esta entrega corresponderá a la segunda parte del proyecto.
+
+Durante la implementación de esta parte del proyecto hemos tenido algunos problemas con los valores que retornaban algunas funciones que posteriormente
+eran recogidos desde otras llamadas a funciones y se trataban de forma incorrecta. Para solucionar esta situación hemos verificado que todas las
+funciones retornaban los valores pertinentes para que las llamadas a estas no creasen estos problemas.
+
+En relación a esto se ha tenido que suprimir el valor de EXIT_FAILURE de la librería otorgada y usar nuestros propios códigos de error
+(0 si todo va bien y 1 si ha habido algún error).
+
+Por otra parte, a diferencia de la anterior entrega del proyecto, se han implementado algunas de las mejoras que contempla la plantilla de corrección:
+    - comando mi_ls mejorado para mostrar más información sobre cada entrada del directorio
+    - implementación del comando mi_touch para la creación de ficheros, dejando mi_mkdir solo para la creación de directorios
+    - escritura desde fichero externo en escribir.c (CORRECCIÓN: a implementar para la siguiente entrega)
+    - mejora del init_MB()
+    - implementación de un buffer de entradas en buscar_entrada() y mi_dir() para reducir las llamadas a lecturas
+    - creación del comando extra mi_rmdir que elimina los directorios a excepción del directorio raíz
---
+++ Entrega 3/simulacion.c
@@ -0,0 +1,73 @@
+#include "directorios.h"
+#include "simulacion.h"
+
+static int acabados = 0;
+
+void reaper(){
+  pid_t ended;
+  signal(SIGCHLD, reaper);
+  while ((ended=waitpid(-1, NULL, WNOHANG))>0) {
+     acabados++;
+  }
+}
+
+int main(int argc,char **argv) {
+    signal(SIGCHLD,reaper);
+    if (argc != 2) {
+        fprintf(stderr,"Sintaxis: ./simulacion <disco>\n");
+        return -1;
+    }
+
+    if (bmount(argv[1]) < 0) return -1;
+
+    char ruta[100];
+    struct tm *ts;
+    time_t timer = time(NULL);
+
+    ts = localtime(&timer);
+    strftime(ruta, sizeof(ruta), "/simul_%Y%m%d%H%M%S/", ts);
+
+    if (mi_creat(ruta,7) == -1) return -1;
+    for (int numProcesos = 1; numProcesos <= NUMPROCESOS;numProcesos++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (bmount(argv[1]) < 0) {
+                return -1;
+            }
+            char idProceso[5];
+            sprintf(idProceso, "%d", getpid());
+            strcat(idProceso,"/");
+            strcat(ruta,"proceso_");
+            strcat(ruta,idProceso);
+            if (mi_creat(ruta,7) == -1) return -1;
+            strcat(ruta,"prueba.dat");
+            if (mi_creat(ruta,7) == -1) return -1;
+            srand(time(NULL) + getpid());
+            int nEscritura = 1;
+            for (;nEscritura <= NUMESCRITURAS;nEscritura++) {
+                struct REGISTRO registro;
+                registro.fecha = time(NULL);
+                registro.pid = getpid();
+                registro.nEscritura = nEscritura;
+                registro.nRegistro = rand() % REGMAX;
+
+                if (mi_write(ruta,&registro,registro.nRegistro*sizeof(struct REGISTRO),sizeof(struct REGISTRO)) == -1) {
+                    fprintf(stderr,"fallo\n");
+                    return -1;
+                }
+                //fprintf(stderr,"simulacion.c -> Escritura %d en %s\n",nEscritura,ruta);
+                usleep(50000);
+            }
+            nEscritura--;
+            fprintf(stderr,"PROCESO %d: Completadas %d escrituras en %s\n",numProcesos,nEscritura,ruta);
+            if (bumount() < 0) return -1;
+            exit(0);
+        }
+        usleep(0.2*1000000);
+    }
+
+    while (acabados < NUMPROCESOS) pause();
+
+    if (bumount() < 0) return -1;
+    exit(0);
+}
